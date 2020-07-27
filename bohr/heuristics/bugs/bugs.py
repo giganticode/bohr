5aa575e4cfaaa2b6e9f95a0a9a7467221186d61e
hlib <hlibbabii@gmail.com>
2020-07-27 22:02:23 +0200

    make heuristics more readable

--- bohr/heuristics/bugs/bugs.py
+++ bohr/heuristics/bugs/bugs.py
@@ -1,79 +1,32 @@
-from bohr.heuristics.templates import regex_git2__for_commit_message, keyword_lookup_template, ISSUE_CONTENTS, \
-    ISSUE_LABELS, COMMIT_MESSAGE
+from bohr.heuristics.templates import regex_git2__for_commit_message, keyword_lookup, COMMIT_MESSAGE_STEMMED, ISSUE_LABELS_STEMMED, ISSUE_CONTENTS_STEMMED
 from bohr.snorkel_utils import BUG
 
-heuristics = [
-    regex_git2__for_commit_message
-] + keyword_lookup_template(label=BUG, terms=[
-    'bad',
-    'broken',
-    'bug',
-    'bugg',
-    'concurr',
-    'correct',
-    'crash',
-    'defect',
-    'error',
-    'fail',
-    'failur',
-    'fix',
-    'hotfix',
-    'invalid',
-    'issue',
-    'quickfix',
-    'unknown',
-]) + keyword_lookup_template(label=BUG, terms=[
-    'except',
-], only_full_word=False) + keyword_lookup_template(label=BUG, terms=[
-    'close',
-    'fault',
-    'handl',
-    'minor',
-], restrict_to_fields=[ISSUE_CONTENTS, ISSUE_LABELS]) + keyword_lookup_template(label=BUG, terms=[
-    'logic',
-    'merg',
-], restrict_to_fields=[ISSUE_LABELS]) + keyword_lookup_template(label=BUG, terms=[
-    'correctli',
-    'corrupt',
-    'deadlock',
-    'dead lock',
-    'endless',
-    'ensur',
-    'garbag',
-    'incorrect',
-    'leak',
-    'fix and test',
-    'incomplet',
-    'inconsist',
-    'infinit',
-    'loop',
-    'mistak',
-    'not return',
-    'not work',
-    'prevent',
-    'problem',
-    'properli',
-    'race condit',
-    'repair',
-    'small fix',
-    'solv',
-    'threw',
-    'throw',
-    'timeout',
-    'unabl',
-    'unclos',
-    'unexpect',
-    'unsynchron',
-    'wrong',
-], restrict_to_fields=[COMMIT_MESSAGE, ISSUE_CONTENTS]) + keyword_lookup_template(label=BUG, terms=[
-    'disabl',
-    'fix & test',
-    'not log',
-    'patch',
-    'resolv',
-], restrict_to_fields=[ISSUE_CONTENTS]) + keyword_lookup_template(label=BUG, terms=[
-    'nullpointer',
-    'null pointer',
-    'outofbound',
-    'out of bound',
-], restrict_to_fields=[COMMIT_MESSAGE, ISSUE_CONTENTS], only_full_word=False)
+heuristics = [regex_git2__for_commit_message] + \
+             [keyword_lookup(k, field=COMMIT_MESSAGE_STEMMED, label=BUG) for k in [
+                 'bad', 'broken', 'bug', 'bugg', 'concurr', 'correct', 'correctli', 'corrupt', 'crash', 'dead lock',
+                 'deadlock', 'defect', 'endless', 'ensur', 'error', 'fail', 'failur', 'fix', 'fix and test', 'garbag',
+                 'hotfix', 'incomplet', 'inconsist', 'incorrect', 'infinit', 'invalid', 'issue', 'leak', 'loop',
+                 'mistak', 'not return', 'not work', 'prevent', 'problem', 'properli', 'quickfix', 'race condit',
+                 'repair', 'small fix', 'solv', 'threw', 'throw', 'timeout', 'unabl', 'unclos', 'unexpect', 'unknown',
+                 'unsynchron', 'wrong']
+              ] + \
+             [keyword_lookup(k, field=ISSUE_LABELS_STEMMED, label=BUG) for k in [
+                 'bad', 'broken', 'bug', 'bugg', 'close', 'concurr', 'correct', 'crash', 'defect', 'error', 'fail',
+                 'failur', 'fault', 'fix', 'handl', 'hotfix', 'invalid', 'issue', 'logic', 'merg', 'minor', 'quickfix',
+                 'unknown']
+              ] + \
+             [keyword_lookup(k, field=ISSUE_CONTENTS_STEMMED, label=BUG) for k in [
+                 'bad', 'broken', 'bug', 'bugg', 'close', 'concurr', 'correct', 'correctli', 'corrupt', 'crash',
+                 'dead lock', 'deadlock', 'defect', 'disabl', 'endless', 'ensur', 'error', 'fail', 'failur',
+                 'fault', 'fix', 'fix & test', 'fix and test', 'garbag', 'handl', 'hotfix', 'incomplet', 'inconsist',
+                 'incorrect', 'infinit', 'invalid', 'issue', 'leak', 'loop', 'minor', 'mistak', 'not log', 'not return',
+                 'not work', 'patch', 'prevent', 'problem', 'properli', 'quickfix', 'race condit', 'repair', 'resolv',
+                 'small fix', 'solv', 'threw', 'throw', 'timeout', 'unabl', 'unclos', 'unexpect', 'unknown',
+                 'unsynchron', 'wrong']] + \
+             [keyword_lookup(k, field=COMMIT_MESSAGE_STEMMED, label=BUG, only_full_word=False) for k in [
+                 'except', 'nullpointer', 'null pointer', 'outofbound', 'out of bound']
+              ] + \
+             [keyword_lookup(k, field=ISSUE_LABELS_STEMMED, label=BUG, only_full_word=False) for k in ['except']] + \
+             [keyword_lookup(k, field=ISSUE_CONTENTS_STEMMED, label=BUG, only_full_word=False) for k in [
+                 'except', 'nullpointer', 'null pointer', 'outofbound', 'out of bound']
+              ]
--- bohr/heuristics/templates.py
+++ bohr/heuristics/templates.py
@@ -1,19 +1,18 @@
 import json
 import re
 from json.decoder import JSONDecodeError
-from typing import List, Optional, Callable
 
 from snorkel.labeling import labeling_function, LabelingFunction
 import pandas as pd
 
 from bohr.snorkel_utils import ABSTAIN, BUG, BUGLESS, Label
 
-COMMIT_MESSAGE = 'commit_message'
-ISSUE_CONTENTS = 'issue_contents'
-ISSUE_LABELS = 'issue_labels'
+COMMIT_MESSAGE_STEMMED = 'commit_message_stemmed'
+ISSUE_CONTENTS_STEMMED = 'issue_contents_stemmed'
+ISSUE_LABELS_STEMMED = 'issue_labels_stemmed'
 
 
-def is_word_in_text(word, text) -> bool:
+def is_word_in_text(word: str, text: str) -> bool:
     """
     Examples:
     ---------
@@ -28,56 +27,18 @@ def is_word_in_text(word, text) -> bool:
     return bool(matches)
 
 
-def keyword_lookup(x: pd.DataFrame, keyword: str, field: str, label: Label, only_full_word=True) -> Label:
-    """
-    Examples:
-    ---------
-    >>> s = pd.Series(data=['fix bug', 'defect'], index=[COMMIT_MESSAGE, ISSUE_LABELS])
-    >>> keyword_lookup(s, 'fix', COMMIT_MESSAGE, BUG)
-    1
-    >>> keyword_lookup(s, 'fi', COMMIT_MESSAGE, BUG)
-    -1
-    >>> keyword_lookup(s, 'fi', COMMIT_MESSAGE, BUG, only_full_word=False)
-    1
-    >>> keyword_lookup(s, 'fix', ISSUE_LABELS, BUG)
-    -1
-    """
-    if x[field]:
-        lowercased_field = str(x[field]).lower()
-        if only_full_word:
-            if is_word_in_text(keyword, lowercased_field):
-                return label
-        elif keyword in lowercased_field:
-            return label
-    return ABSTAIN
-
-
-def keyword_lookup_template(terms: List[str], label: int, restrict_to_fields: Optional[List[str]] = None, only_full_word=True) -> List[Callable]:
-    restrict_to_fields = restrict_to_fields or [COMMIT_MESSAGE, ISSUE_CONTENTS, ISSUE_LABELS]
-
-    labeling_functions = []
-    for term in terms:
-        for field in restrict_to_fields:
-            labeling_functions.append(LabelingFunction(
-                name= f'{term}__for_{field}',
-                f=keyword_lookup,
-                resources=dict(keyword=term, field=f'{field}_stemmed', label=label, only_full_word=only_full_word)
-            ))
-    return labeling_functions
-
-
 @labeling_function()
-def regex_git2__for_commit_message(x: pd.Series):
+def regex_git2__for_commit_message(x: pd.Series) -> Label:
     return BUG if re.search(r"gh(-|\s)\d+", x.commit_message_stemmed, flags=re.I) else ABSTAIN
 
 
 @labeling_function()
-def regex_version__for_commit_message(x):
+def regex_version__for_commit_message(x: pd.Series) -> Label:
     return BUGLESS if re.search(r"v\d+.*", x.commit_message_stemmed, flags=re.I) else ABSTAIN
 
 
 @labeling_function()
-def fix_bugless__for_commit_message(x):
+def fix_bugless__for_commit_message(x: pd.Series) -> Label:
     if is_word_in_text('fix', x.commit_message_stemmed.lower()):
         if any(is_word_in_text(word, x.commit_message_stemmed.lower()) for word in ['ad', 'add', 'build', 'chang', 'doc', 'document', 'javadoc', 'junit', 'messag', 'test', 'typo', 'unit', 'warn']):
             return BUGLESS
@@ -87,7 +48,7 @@ def fix_bugless__for_commit_message(x):
 
 
 @labeling_function()
-def bug_bugless__for_commit_message(x):
+def bug_bugless__for_commit_message(x: pd.Series) -> Label:
     if is_word_in_text('bug', x.commit_message_stemmed.lower()):
         if any(is_word_in_text(word, x.commit_message_stemmed.lower()) for word in ['ad', 'add', 'chang', 'doc', 'document', 'javadoc', 'junit', 'report', 'test', 'typo', 'unit']):
             return BUGLESS
@@ -96,13 +57,43 @@ def bug_bugless__for_commit_message(x):
     return ABSTAIN
 
 
-@labeling_function()
-def no_files_have_modified_status(x) -> bool:
-    try:
-        for file in json.loads(x.file_details):
-            if file['status'] == 'modified':
-                return ABSTAIN
-        return BUGLESS
-    except JSONDecodeError:
-        return ABSTAIN
+def no_files_have_modified_status(label: Label) -> LabelingFunction:
 
+    @labeling_function(name='no_files_have_modified_status')
+    def lf(x: pd.Series) -> Label:
+        try:
+            for file in json.loads(x.file_details):
+                if file['status'] == 'modified':
+                    return ABSTAIN
+            return label
+        except JSONDecodeError:
+            return ABSTAIN
+
+    return lf
+
+
+def keyword_lookup(keyword: str, field: str, label: Label, only_full_word=True) -> LabelingFunction:
+    """
+    Examples:
+    ---------
+    >>> s = pd.Series(data=['fix bug', 'defect'], index=[COMMIT_MESSAGE_STEMMED, ISSUE_LABELS_STEMMED])
+    >>> keyword_lookup('fix', COMMIT_MESSAGE_STEMMED, BUG)(s)
+    1
+    >>> keyword_lookup('fi', COMMIT_MESSAGE_STEMMED, BUG)(s)
+    -1
+    >>> keyword_lookup('fi', COMMIT_MESSAGE_STEMMED, BUG, only_full_word=False)(s)
+    1
+    >>> keyword_lookup('fix', ISSUE_LABELS_STEMMED, BUG)(s)
+    -1
+    """
+    @labeling_function(name=f'{keyword}__for_{field}')
+    def lf(x: pd.Series) -> Label:
+        if x[field]:
+            lowercased_field = str(x[field]).lower()
+            if only_full_word:
+                if is_word_in_text(keyword, lowercased_field):
+                    return label
+            elif keyword in lowercased_field:
+                return label
+        return ABSTAIN
+    return lf

bcd9ab07c492fb13771001225b5ec3ed8e8c7efe
Hlib <hlibbabii@gmail.com>
2020-10-03 16:50:09 +0000

    increase the training set to 100000 rows

--- bohr/pipeline/apply_heuristics.py
+++ bohr/pipeline/apply_heuristics.py
@@ -34,7 +34,7 @@ def majority_acc(L: np.ndarray, df: dd.DataFrame) -> float:
 def apply_heuristics(args) -> Dict[str, Any]:
     stats: Dict[str, Any] = {}
 
-    df_train = pd.read_csv(TRAIN_DIR / 'b_b.csv', nrows=args.rows_train)
+    df_train = pd.read_csv(TRAIN_DIR / 'b_b.csv', nrows=100000)
     df_test = pd.concat([pd.read_csv(p)
                          for p in TEST_DIR.glob('*.csv')], ignore_index=True, sort=True)
 
@@ -74,7 +74,6 @@ def apply_heuristics(args) -> Dict[str, Any]:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--rows-train', type=int, default=50000)
     parser.add_argument('--save-heuristics-matrix-train-to',
                         default='generated/heuristic_matrix_train.pkl')
     parser.add_argument('--save-heuristics-matrix-test-to',

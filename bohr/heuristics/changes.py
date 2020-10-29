fc265092d38bb780cea3faa700c3ea078d59c2f4
Hlib <hlibbabii@gmail.com>
2020-10-29 23:38:36 +0100

    new change heuristics: bugless if many files; bug if only changed lines in one file (#41)

--- bohr/heuristics/changes.py
+++ bohr/heuristics/changes.py
@@ -5,5 +5,20 @@ from bohr.snorkel_utils import Commit, Label, commit_lf
 def no_files_have_modified_status(commit: Commit) -> Label:
     for file in commit.files:
         if file.status == 'modified': return Label.ABSTAIN
-        if file.status == 'added': return Label.ABSTAIN
     return Label.BUGLESS
+
+
+@commit_lf()
+def bug_if_only_changed_lines_in_one_file(commit: Commit) -> Label:
+    if len(commit.files) == 1 and commit.files[0].status == 'modified' \
+            and commit.files[0].changes and commit.files[0].no_added_lines() and commit.files[0].no_removed_lines():
+        return Label.BUG
+    return Label.ABSTAIN
+
+
+@commit_lf()
+def bugless_if_many_files_changes(commit: Commit) -> Label:
+    if len(commit.files) > 6:
+        return Label.BUGLESS
+    else:
+        return Label.ABSTAIN

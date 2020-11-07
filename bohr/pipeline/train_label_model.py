735a05454fc05194b9e3ebc58772a561325cf803
hlib <hlibbabii@gmail.com>
2020-11-07 19:40:30 +0100

    make use of dvc params and separate params file instead of passing command line args

--- bohr/pipeline/train_label_model.py
+++ bohr/pipeline/train_label_model.py
@@ -1,4 +1,3 @@
-import argparse
 import json
 from pathlib import Path
 from pprint import pprint
@@ -8,7 +7,7 @@ import numpy as np
 import pandas as pd
 from snorkel.labeling.model import LabelModel
 
-from bohr import PROJECT_DIR, TEST_DIR
+from bohr import PROJECT_DIR, TEST_DIR, params
 
 
 def get_test_set_accuracy(label_model: LabelModel, test_set: str, save_to: Path) -> float:
@@ -17,10 +16,10 @@ def get_test_set_accuracy(label_model: LabelModel, test_set: str, save_to: Path)
     return label_model.score(L=L, Y=df.bug, tie_break_policy="random")["accuracy"]
 
 
-def train_label_model(args) -> Dict[str, Any]:
+def train_label_model() -> Dict[str, Any]:
     stats: Dict[str, Any] = {}
 
-    task_dir_generated = PROJECT_DIR / 'generated' / args.task
+    task_dir_generated = PROJECT_DIR / 'generated' / params.TASK
 
     L_train = np.load(task_dir_generated / 'heuristic_matrix_train.pkl', allow_pickle=True)
     label_model = LabelModel(cardinality=2, verbose=True)
@@ -28,19 +27,15 @@ def train_label_model(args) -> Dict[str, Any]:
     label_model.save(task_dir_generated / 'label_model.pkl')
     label_model.eval()
 
-    for test_set in ['herzig', 'berger', '1151-commits']:
+    for test_set in params.TEST_SETS:
         stats[f'label_model_acc_{test_set}'] = get_test_set_accuracy(label_model, test_set, save_to=task_dir_generated)
 
     return stats
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('task', type=str)
-    args = parser.parse_args()
-
-    stats = train_label_model(args)
-    with open(PROJECT_DIR / 'metrics' / args.task / 'label_model_metrics.json', 'w') as f:
+    stats = train_label_model()
+    with open(PROJECT_DIR / 'metrics' / params.TASK / 'label_model_metrics.json', 'w') as f:
         json.dump(stats, f)
 
     pprint(stats)

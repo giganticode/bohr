290fe18f489d9616810eca1971ed0a461b1fb5b8
GitHub Action <action@github.com>
2020-11-08 22:23:04 +0000

    Add dvc outputs

--- bohr/labels.py
+++ bohr/labels.py
@@ -1,7 +1,60 @@
-from enum import Enum
+# This is automatically generated code. Do not edit manually.
+class Label(object):
+    pass
 
 
-class Label(Enum):
-    BUG = 1
-    BUGLESS = 0
-    ABSTAIN = -1
+class TangledCommit(Label):
+    pass
+
+
+class NonTangledCommit(Label):
+    pass
+
+
+class Bugless(Label):
+    pass
+
+
+class Minor(Label):
+    pass
+
+
+class Major(Label):
+    pass
+
+
+class Critical(Label):
+    pass
+
+
+class BugFix(Label):
+    pass
+
+
+class NonBugFix(Label):
+    pass
+
+
+class BogusFix(NonBugFix):
+    pass
+
+
+class DocFix(BogusFix):
+    pass
+
+
+class TestFix(BogusFix):
+    pass
+
+
+TangledCommit = TangledCommit()
+NonTangledCommit = NonTangledCommit()
+Bugless = Bugless()
+Minor = Minor()
+Major = Major()
+Critical = Critical()
+BugFix = BugFix()
+NonBugFix = NonBugFix()
+BogusFix = BogusFix()
+DocFix = DocFix()
+TestFix = TestFix()

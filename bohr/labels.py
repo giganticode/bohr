8d105c8f48f66263cde903d1a7c3020ef8ec2d7c
hlib <hlibbabii@gmail.com>
2020-11-08 21:54:49 +0100

    Revert "Add dvc outputs"
    This reverts commit 210f9704fd8f0e9de49b48232ebb7b0b5fef7807.

--- bohr/labels.py
+++ bohr/labels.py
@@ -1,60 +1,7 @@
-# This is automatically generated code. Do not edit manually.
-class Label(object):
-    pass
+from enum import Enum
 
 
-class TangledCommit(Label):
-    pass
-
-
-class NonTangledCommit(Label):
-    pass
-
-
-class Bugless(Label):
-    pass
-
-
-class Minor(Label):
-    pass
-
-
-class Major(Label):
-    pass
-
-
-class Critical(Label):
-    pass
-
-
-class BugFix(Label):
-    pass
-
-
-class NonBugFix(Label):
-    pass
-
-
-class BogusFix(NonBugFix):
-    pass
-
-
-class DocFix(BogusFix):
-    pass
-
-
-class TestFix(BogusFix):
-    pass
-
-
-TangledCommit = TangledCommit()
-NonTangledCommit = NonTangledCommit()
-Bugless = Bugless()
-Minor = Minor()
-Major = Major()
-Critical = Critical()
-BugFix = BugFix()
-NonBugFix = NonBugFix()
-BogusFix = BogusFix()
-DocFix = DocFix()
-TestFix = TestFix()
+class Label(Enum):
+    BUG = 1
+    BUGLESS = 0
+    ABSTAIN = -1

c91e2f63df22022ab5a7e2ffa9fd62a53229a525
Hlib <hlibbabii@gmail.com>
2021-01-03 22:10:22 +0100

    Move labeled datasets to a separate directory (#70)
    * move labeled datasets to a separate directory
    
    * create labeled-data dir if doen't exist
    
    * add missing .csv
    
    * log the location to where labeled dataset was written to
    
    * Add dvc outputs
    
    * Add dvc outputs
    
    Co-authored-by: GitHub Action <action@github.com>

--- bohr/__init__.py
+++ bohr/__init__.py
@@ -9,6 +9,7 @@ TEST_DIR = DATA_DIR / "test"
 TRAIN_DIR = DATA_DIR / "train"
 HEURISTIC_DIR = PROJECT_DIR / "bohr" / "heuristics"
 DATASET_DIR = PROJECT_DIR / "bohr" / "datasets"
+LABELED_DATA_DIR = PROJECT_DIR / "labeled-data"
 
 
 FORMAT = "%(message)s"
--- bohr/pipeline/label_dataset.py
+++ bohr/pipeline/label_dataset.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from snorkel.labeling.model import LabelModel
 
-from bohr import PROJECT_DIR
+from bohr import PROJECT_DIR, LABELED_DATA_DIR
 from bohr.core import (
     load_heuristics,
     DatasetLoader,
@@ -48,7 +48,11 @@ def label_dataset(task_name: str, dataset_name: str, debug: bool = False):
         col_lfs = df_lfs.apply(lambda c: ";".join([v for v in c if v]), axis=1)
         df_labeled["lfs"] = col_lfs
 
-    df_labeled.to_csv("labeled_with_model2.csv", index=False)
+    if not LABELED_DATA_DIR.exists():
+        LABELED_DATA_DIR.mkdir(parents=True)
+    target_file = LABELED_DATA_DIR / f"{dataset_name}.csv"
+    df_labeled.to_csv(target_file, index=False)
+    print(f"Labeled dataset has been written to {target_file}.")
 
 
 if __name__ == "__main__":

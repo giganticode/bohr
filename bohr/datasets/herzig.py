e822e7d9cfa76eed8d2f256baa98789703ab73d8
Hlib <hlibbabii@gmail.com>
2021-01-03 22:11:53 +0100

    Add mapper as DataLoader field (#71)
    * add mapper as as DataLoader field
    
    * Add dvc outputs
    
    * Add dvc outputs
    
    Co-authored-by: GitHub Action <action@github.com>

--- bohr/datasets/herzig.py
+++ bohr/datasets/herzig.py
@@ -1,7 +1,10 @@
 from bohr import DATA_DIR
 from bohr.datasets.templates.loaders.from_csv import CsvDatasetLoader
-
+from bohr.datasets.templates.mappers.commit import CommitMapper
 
 dataset_loader = CsvDatasetLoader(
-    "herzig", path_to_file=DATA_DIR / "test" / "herzig.csv", test_set=True
+    "herzig",
+    path_to_file=DATA_DIR / "test" / "herzig.csv",
+    mapper=CommitMapper(),
+    test_set=True,
 )

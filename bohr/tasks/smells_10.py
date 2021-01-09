e24b154b11164b9bdb7e4c77a725c84727131982
hlib <hlibbabii@gmail.com>
2021-01-09 17:30:55 +0100

    add smells short task

--- bohr/tasks/smells_10.py
+++ bohr/tasks/smells_10.py
@@ -1,5 +1,5 @@
 top_artifact = "method.Method"
 label_categories = ["SnippetLabel.NonSmelly", "SnippetLabel.Smelly"]
-test_datasets = ["smells-test"]
-train_datasets = ["smells-train"]
+test_datasets = ["smells-test_10"]
+train_datasets = ["smells-train_10"]
 label_column_name = "smelly"

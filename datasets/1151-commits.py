6e8528e20ac08003000b02734e084128e840e0c8
hlib <hlibbabii@gmail.com>
2021-02-21 12:39:40 +0100

    use v 0.1 of bohr framework

--- datasets/1151-commits.py
+++ datasets/1151-commits.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+
 from bohr.templates.dataloaders.from_csv import CsvDatasetLoader
 from bohr.templates.datamappers.commit import CommitMapper
 
 dataset_loader = CsvDatasetLoader(
-    "1151-commits",
     path_to_file="data/bugginess/test/1151-commits.csv",
-    mapper=CommitMapper(),
+    mapper=CommitMapper(Path(__file__).parent.parent),
     test_set=True,
 )
+
+__all__ = [dataset_loader]

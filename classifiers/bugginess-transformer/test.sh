ccc13a35e963ac988af4d3697391985fd7b5d04e
hlib <hlibbabii@gmail.com>
2021-03-03 14:00:08 +0100

    add dvc stages for transformer

--- classifiers/bugginess-transformer/test.sh
+++ classifiers/bugginess-transformer/test.sh
@@ -1,8 +1,8 @@
-python classifiers/bugginess-transformer/run.py --model_name_or_path bohr_model/ \
+python classifiers/bugginess-transformer/run.py --model_name_or_path models \
               --do_eval \
               --evaluation_strategy steps \
               --eval_steps 0 \
               --eval_test \
-              --output_dir /tmp \
-	            --per_device_eval_batch_size 14 \
-              --data_file $1
+              --output_dir "$2" \
+	          --per_device_eval_batch_size 14 \
+              --data_file "$1"
--- classifiers/bugginess-transformer/train.sh
+++ classifiers/bugginess-transformer/train.sh
@@ -1,13 +1,13 @@
 python classifiers/bugginess-transformer/run.py --model_name_or_path "giganticode/StackOBERTflow-comments-small-v1" \
-              --output_dir bohr_model \
+              --output_dir models \
               --do_train \
               --do_eval \
               --per_device_train_batch_size 14 \
               --overwrite_output_dir \
               --save_steps 4000 \
-              --num_train_epochs 3 \
+              --num_train_epochs 1 \
               --logging_steps 4000 \
               --eval_steps 4000 \
               --evaluation_strategy steps \
               --data_file labeled-data/bugginess.csv \
-               --fp16
+              --fp16

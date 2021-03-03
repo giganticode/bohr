87ca634573f9141545ff4b22a2f0583a5de5e73e
Hlib <hlibbabii@gmail.com>
2021-03-03 16:07:08 +0100

    add transformer outputs

--- classifiers/bugginess-transformer/test.sh
+++ classifiers/bugginess-transformer/test.sh
@@ -4,5 +4,5 @@ python classifiers/bugginess-transformer/run.py --model_name_or_path models \
               --eval_steps 0 \
               --eval_test \
               --output_dir "$2" \
-	          --per_device_eval_batch_size 14 \
+	      --per_device_eval_batch_size 14 \
               --data_file "$1"

4c8b63e4a99096737711add1c95263b21a726284
Hlib <hlibbabii@gmail.com>
2021-03-17 20:02:27 +0100

    Update/label with transformer (#157)
    * make transformer label things
    
    * add outputs
    
    * combine label model and transformer labels

---
+++ classifiers/bugginess-transformer/combine_labels.py
@@ -0,0 +1,20 @@
+import sys
+from pathlib import Path
+
+import pandas as pd
+
+from bohr.config import load_config
+
+
+def combine_labels(path_to_labeled_dataset: Path, path_to_transformer_labels, output_path: Path) -> None:
+    labeled_dataset = pd.read_csv(path_to_labeled_dataset)
+    transformer_labels = pd.read_csv(path_to_transformer_labels)['prediction'].rename('transformer_preds')
+    combined = pd.concat([labeled_dataset, transformer_labels], axis=1)
+    combined.to_csv(output_path)
+
+
+if __name__ == '__main__':
+    project_root = load_config().project_root
+    combine_labels(project_root / Path(sys.argv[1]),
+                   project_root / Path(sys.argv[2]),
+                   project_root / Path(sys.argv[3]))

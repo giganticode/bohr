ca30546bde71a7dada8fba95373b08c74525b809
hlib <hlibbabii@gmail.com>
2021-03-30 22:27:48 +0200

    add dvc outputs

--- classifiers/bugginess-transformer/combine_labels.py
+++ classifiers/bugginess-transformer/combine_labels.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pandas as pd
 
-from bohr.config import load_config
+from bohr.pathconfig import load_path_config
 
 
 def combine_labels(path_to_labeled_dataset: Path, path_to_transformer_labels, output_path: Path) -> None:
@@ -14,7 +14,7 @@ def combine_labels(path_to_labeled_dataset: Path, path_to_transformer_labels, ou
 
 
 if __name__ == '__main__':
-    project_root = load_config().project_root
+    project_root = load_path_config().project_root
     combine_labels(project_root / Path(sys.argv[1]),
                    project_root / Path(sys.argv[2]),
                    project_root / Path(sys.argv[3]))

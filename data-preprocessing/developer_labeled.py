9022481048d21004260a56f5ed800d202ff128c0
hlib <hlibbabii@gmail.com>
2021-06-23 17:07:19 +0200

    fix developer labeled dataset

--- data-preprocessing/developer_labeled.py
+++ data-preprocessing/developer_labeled.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
+import tempfile
 from pathlib import Path
 
 import pandas as pd
@@ -8,38 +10,44 @@ import pandas as pd
 path_to_file = os.path.realpath(__file__)
 repo_root = Path(path_to_file).parent.parent
 
-DIR = repo_root / "downloaded-data" / "developer-labeled"
+INPUT_ZIP = repo_root / "downloaded-data" / "developer-labeled-commits.zip"
 SAVE_TO = repo_root / "data" / "developer-labeled.csv"
 
-commits = pd.read_csv(
-    DIR / "Commits.csv",
-    names=["id", "sha", "project", "author", "committer", "date", "message"],
-)
-# projects = pd.read_csv(DIR / 'Projects.csv', names=['id', 'date', 'url'])
-survey_results = pd.read_csv(
-    DIR / "SurveyResults.csv",
-    names=[
-        "id",
-        "sw_adaptive",
-        "sw_corrective",
-        "sw_perfective",
-        "nfr_maintainability",
-        "nfr_usability",
-        "nfr_functionality",
-        "nfr_reliability",
-        "nfr_efficiency",
-        "nfr_portability",
-        "nfr_none",
-        "hl_forward",
-        "hl_reengineering",
-        "hl_corrective",
-        "hl_management",
-    ],
-)
+with tempfile.TemporaryDirectory() as dir:
+    subprocess.run(["unzip", INPUT_ZIP, "-d", dir])
+
+    commits = pd.read_csv(
+        Path(dir) / "msr-data-master" / "Commits.csv",
+        names=["id", "sha", "project", "author", "committer", "date", "message"],
+    )
+    # projects = pd.read_csv(DIR / 'Projects.csv', names=['id', 'date', 'url'])
+    survey_results = pd.read_csv(
+        Path(dir) / "msr-data-master" / "SurveyResults.csv",
+        names=[
+            "id",
+            "sw_adaptive",
+            "sw_corrective",
+            "sw_perfective",
+            "nfr_maintainability",
+            "nfr_usability",
+            "nfr_functionality",
+            "nfr_reliability",
+            "nfr_efficiency",
+            "nfr_portability",
+            "nfr_none",
+            "hl_forward",
+            "hl_reengineering",
+            "hl_corrective",
+            "hl_management",
+        ],
+    )
 
 merged = pd.merge(commits, survey_results, on="id")
 
 result = merged[["sha", "message", "sw_corrective"]]
 result.columns = ["sha", "message", "bug"]
+
+result["owner"] = ""
+result["repository"] = ""
 
 result.to_csv(SAVE_TO, index_label="commit_id")

6286763cab692f5fdca4bff47a6efa082fa83e81
hlib <hlibbabii@gmail.com>
2021-08-28 11:37:39 +0200

    feat: more heuristics

--- heuristics/bugginess_files.py
+++ heuristics/bugginess_files.py
@@ -1,3 +1,4 @@
+import re
 from typing import Optional
 
 from bohr.collection.artifacts import Commit
@@ -211,3 +212,41 @@ def refactorings_detected(
         if len(refactoring_miner_output.commits[0].refactorings) > 0:
             return CommitLabel.Refactoring
     return None
+
+
+@Heuristic(Commit)
+def all_files_test_add(commit: Commit) -> Optional[Labels]:
+    TEST_FILE_REGEX = re.compile(r"test", flags=re.I)
+    if len(commit.commit_files) == 0:
+        return None
+
+    for file in commit.commit_files:
+
+        def only_additions():
+            return not (
+                not file.changes or "<re>" in file.changes or "<del>" in file.changes
+            )
+
+        if not (
+            TEST_FILE_REGEX.match(str(file.filename))
+            and (file.status == "added" or only_additions())
+        ):
+            return None
+    return CommitLabel.TestAdd
+
+
+@Heuristic(Commit)
+def all_files_test_fix(commit: Commit) -> Optional[Labels]:
+    TEST_FILE_REGEX = re.compile(r"test", flags=re.I)
+    if len(commit.commit_files) == 0:
+        return None
+
+    for file in commit.commit_files:
+        if not (
+            TEST_FILE_REGEX.match(str(file.filename))
+            and file.status == "modified"
+            and file.changes
+            and "<re>" in file.changes
+        ):
+            return None
+    return CommitLabel.TestFix
---
+++ heuristics/commit_explorer.py
@@ -0,0 +1,56 @@
+from typing import Optional
+
+import labels as l
+from bohr.collection.artifacts import Commit
+from bohr.core import Heuristic
+from bohr.labeling.labelset import Labels
+
+
+@Heuristic(Commit)
+def commit_explorer_output_merge(commit: Commit) -> Optional[Labels]:
+    if commit.commit_explorer_data is None:
+        return None
+
+    if (
+        "special_commit_finder/0_1" in commit.commit_explorer_data
+        and commit.commit_explorer_data["special_commit_finder/0_1"]["merge"]
+    ):
+        return l.CommitLabel.Merge
+
+
+@Heuristic(Commit)
+def commit_explorer_output_init(commit: Commit) -> Optional[Labels]:
+    if commit.commit_explorer_data is None:
+        return None
+
+    if (
+        "special_commit_finder/0_1" in commit.commit_explorer_data
+        and commit.commit_explorer_data["special_commit_finder/0_1"]["initial"]
+    ):
+        return l.CommitLabel.InitialCommit
+
+
+# @Heuristic(Commit)
+# def commit_explorer_output_refactoring_miner(commit: Commit) -> Optional[Labels]:
+#     if commit.commit_explorer_data is None:
+#         return None
+#
+#     data = commit.commit_explorer_data
+#     if "refactoring_miner/2_1_0" in data:
+#         if data["refactoring_miner/2_1_0"]:
+#             if data["refactoring_miner/2_1_0"]['status'] == 'ok':
+#                 if len(data["refactoring_miner/2_1_0"]["refactorings"]) == 1:
+#                     if data["refactoring_miner/2_1_0"]["refactorings"][0]["type"] in ['Move Class']:
+#                         print("Contains refactoring")
+#                         return l.CommitLabel.Refactoring
+
+
+@Heuristic(Commit)
+def commit_explorer_output_sstubs(commit: Commit) -> Optional[Labels]:
+    if commit.commit_explorer_data is None:
+        return None
+
+    data = commit.commit_explorer_data
+    if "mine_sstubs/head" in data and data["mine_sstubs/head"]:
+        print("Contains bug")
+        return l.CommitLabel.BugFix

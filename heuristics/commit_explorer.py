428d13b81c5bb6b17d83a98743bee181c1c7fadf
hlib <hlibbabii@gmail.com>
2021-08-28 13:58:45 +0200

    fix: handle exceptions thrown by commit-explorer

--- heuristics/commit_explorer.py
+++ heuristics/commit_explorer.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from commitexplorer.client import CommitExplorerClientException, CommitNotFoundException
+
 import labels as l
 from bohr.collection.artifacts import Commit
 from bohr.core import Heuristic
@@ -8,26 +10,32 @@ from bohr.labeling.labelset import Labels
 
 @Heuristic(Commit)
 def commit_explorer_output_merge(commit: Commit) -> Optional[Labels]:
-    if commit.commit_explorer_data is None:
-        return None
+    try:
+        if commit.commit_explorer_data is None:
+            return None
 
-    if (
-        "special_commit_finder/0_1" in commit.commit_explorer_data
-        and commit.commit_explorer_data["special_commit_finder/0_1"]["merge"]
-    ):
-        return l.CommitLabel.Merge
+        if (
+            "special_commit_finder/0_1" in commit.commit_explorer_data
+            and commit.commit_explorer_data["special_commit_finder/0_1"]["merge"]
+        ):
+            return l.CommitLabel.Merge
+    except (CommitExplorerClientException, CommitNotFoundException) as ex:
+        return None
 
 
 @Heuristic(Commit)
 def commit_explorer_output_init(commit: Commit) -> Optional[Labels]:
-    if commit.commit_explorer_data is None:
-        return None
+    try:
+        if commit.commit_explorer_data is None:
+            return None
 
-    if (
-        "special_commit_finder/0_1" in commit.commit_explorer_data
-        and commit.commit_explorer_data["special_commit_finder/0_1"]["initial"]
-    ):
-        return l.CommitLabel.InitialCommit
+        if (
+            "special_commit_finder/0_1" in commit.commit_explorer_data
+            and commit.commit_explorer_data["special_commit_finder/0_1"]["initial"]
+        ):
+            return l.CommitLabel.InitialCommit
+    except (CommitExplorerClientException, CommitNotFoundException) as ex:
+        return None
 
 
 # @Heuristic(Commit)
@@ -47,10 +55,13 @@ def commit_explorer_output_init(commit: Commit) -> Optional[Labels]:
 
 @Heuristic(Commit)
 def commit_explorer_output_sstubs(commit: Commit) -> Optional[Labels]:
-    if commit.commit_explorer_data is None:
-        return None
+    try:
+        if commit.commit_explorer_data is None:
+            return None
 
-    data = commit.commit_explorer_data
-    if "mine_sstubs/head" in data and data["mine_sstubs/head"]:
-        print("Contains bug")
-        return l.CommitLabel.BugFix
+        data = commit.commit_explorer_data
+        if "mine_sstubs/head" in data and data["mine_sstubs/head"]:
+            print("Contains bug")
+            return l.CommitLabel.BugFix
+    except (CommitExplorerClientException, CommitNotFoundException) as ex:
+        return None

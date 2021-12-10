dddbe7ba63a14c718d08e7c88b166f90980fec05
hlib <hlibbabii@gmail.com>
2021-12-10 17:12:49 +0100

    add transformer heuristic

---
+++ heuristics/bugginess/fine_grained_changes_transformer.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from bohrapi.artifacts import Commit
+from bohrapi.core import Heuristic
+from bohrlabels.core import Labels
+from bohrlabels.labels import CommitLabel
+
+label_map = {
+    "build": CommitLabel.NonBugFix,
+    "chore": CommitLabel.NonBugFix,
+    "ci": CommitLabel.NonBugFix,
+    "docs": CommitLabel.NonBugFix,
+    "feat": CommitLabel.Feature,
+    "fix": CommitLabel.BugFix,
+    "perf": CommitLabel.NonBugFix,
+    "refactor": CommitLabel.Refactoring,
+    "style": CommitLabel.NonBugFix,
+    "test": CommitLabel.NonBugFix
+}
+
+
+@Heuristic(Commit)
+def fine_grained_changes_transformer(commit: Commit) -> Optional[Labels]:
+    if "bohr" in commit.raw_data and "change_transformer_label/0_1" in commit.raw_data["bohr"]:
+        return label_map[commit.raw_data["bohr"]["change_transformer_label/0_1"]]

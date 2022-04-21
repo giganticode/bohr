939bf8004abd1c82ad350787c2ce7996d5c39cdf
hlib <hlibbabii@gmail.com>
2022-04-21 12:40:51 +0200

    fix broken (due to chnaged api) unit tests in heursitics

--- heuristics/bugginess/keywords/github_ref_in_message.py
+++ heuristics/bugginess/keywords/github_ref_in_message.py
@@ -12,13 +12,13 @@ GITHUB_REF_RE = re.compile(r"gh(-|\s)\d+", flags=re.I)
 @Heuristic(Commit)
 def github_ref_in_message(commit: Commit) -> Optional[Labels]:
     """
-    >>> github_ref_in_message(Commit("x", "y", "12afbc4564ba", "gh-123: bug"))
+    >>> github_ref_in_message(Commit({"owner": "x", "repository": "y", "_id": "12afbc4564ba", "message": "gh-123: bug"}))
     CommitLabel.BugFix
-    >>> github_ref_in_message(Commit("x", "y", "12afbc4564ba", "gh 123"))
+    >>> github_ref_in_message(Commit({"owner": "x", "repository": "y", "_id": "12afbc4564ba", "message": "gh 123"}))
     CommitLabel.BugFix
-    >>> github_ref_in_message(Commit("x", "y", "12afbc4564ba", "GH 123: bug2"))
+    >>> github_ref_in_message(Commit({"owner": "x", "repository": "y", "_id": "12afbc4564ba", "message": "GH 123: bug2"}))
     CommitLabel.BugFix
-    >>> github_ref_in_message(Commit("x", "y", "12afbc4564ba", "GH123: wrong issue reference")) is None
+    >>> github_ref_in_message(Commit({"owner": "x", "repository": "y", "_id": "12afbc4564ba", "message": "GH123: wrong issue reference"})) is None
     True
     """
     return CommitLabel.BugFix if GITHUB_REF_RE.search(commit.message.raw) else None
